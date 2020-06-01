2b54e1d37cfef76c6d69dcbb9ac85a71cc2d1ce2
Ilya Konstantinov <ilykonst95@gmail.com>
2020-06-01 09:37:41 +0400

    feat(index.ts): add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import './LoadEnv'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
+import {createConnection} from "typeorm";
 
-// Start the server
-const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
-    logger.info(`Express server started on : http://localhost:${port}`);
-});
+createConnection()
+    .then(() => {
+        const port = Number(process.env.PORT || 3000);
+
+        app.listen(port, () => {
+            logger.info(`Express server started on : http://localhost:${port}`);
+        });
+    });
