0bff70dcc576c05e6c41e99ab7d042ecffdbe868
Ilya Konstantinov <ilykonst95@gmail.com>
2020-06-04 10:49:20 +0400

    style(AdminMiddleWare.ts)

--- src/middlewares/AdminMiddleWare.ts
+++ src/middlewares/AdminMiddleWare.ts
@@ -2,7 +2,6 @@ import {ExpressMiddlewareInterface} from "routing-controllers";
 import {inject, injectable} from "tsyringe";
 import {IJwtService} from "@/services/IJwtService";
 import {cookieProps} from "@shared/constants";
-import {container} from "@/services/serviceContainer";
 import {UserRoles} from "@entities/UserRoles";
 import {UNAUTHORIZED} from "http-status-codes";
 import {NextFunction, Request, Response} from "express";
@@ -16,21 +15,24 @@ export class AdminMiddleWare implements ExpressMiddlewareInterface {
     }
 
     async use(req: Request, res: Response, next: NextFunction) {
+        const jwtError = "JWT not present in signed cookie.";
+
         try {
             // Get json-web-token
             const jwt = req.signedCookies[cookieProps.key];
+
             if (!jwt) {
-                throw Error('JWT not present in signed cookie.');
+                throw Error(jwtError);
             }
             // Make sure user role is an admin
-            const jwtService = container.resolve<IJwtService>("IJwtService");
 
-            const clientData = await jwtService.decode(jwt);
+            const clientData = await this.jwt.decode(jwt);
+
             if (clientData.role === UserRoles.Admin) {
                 res.locals.userId = clientData.id;
                 next();
             } else {
-                throw Error('JWT not present in signed cookie.');
+                throw Error(jwtError);
             }
         } catch (err) {
             return res.status(UNAUTHORIZED).json({
