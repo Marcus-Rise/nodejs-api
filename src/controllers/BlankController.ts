8da6d409b3d5df498b3726fd49472ad64d62ea3c
Ilya Konstantinov <ilykonst95@gmail.com>
2020-06-05 23:30:24 +0400

    feat(BlankController.ts)

---
+++ src/controllers/BlankController.ts
@@ -0,0 +1,19 @@
+import {BaseController} from "@/controllers/BaseController";
+import {Get, JsonController, Res} from "routing-controllers";
+import {Response} from "express";
+import {IUser} from "@/models/IUser";
+import User from "@entities/User.entity";
+
+@JsonController()
+export class BlankController extends BaseController {
+    @Get()
+    async get(@Res() res: Response) {
+        let array: IUser[] = [];
+
+        for (const item of new Array(100)) {
+            array.push(new User());
+        }
+
+        return this.ok<IUser[]>(res, array);
+    }
+}
