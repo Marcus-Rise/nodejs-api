e02e8a6f5c15e827a4a510163951b6879a603b12
Ilya Konstantinov <ilykonst95@gmail.com>
2020-06-06 14:57:29 +0400

    feat(Db.ts): use postgres by default

--- src/Db.ts
+++ src/Db.ts
@@ -2,11 +2,11 @@ import {createConnection} from "typeorm";
 import {ConnectionOptions} from "typeorm/connection/ConnectionOptions";
 
 export const connectToDb = async () => createConnection(<ConnectionOptions>{
-    type: process.env.DB_TYPE || "mongodb",
-    database: process.env.DB_NAME || "test",
+    type: process.env.DB_TYPE || "postgres",
+    database: process.env.DB_NAME || "postgres",
     host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 27017,
-    username: process.env.DB_USER_NAME || "root",
+    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER_NAME || "postgres",
     password: process.env.DB_USER_PASSWORD || "example",
     synchronize: process.env.DB_SYNCHRONIZE === "true",
     logging: process.env.DB_LOGGING === "true",
