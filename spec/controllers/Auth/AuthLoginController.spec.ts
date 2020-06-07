7dd7d2687d01f350189e8d0cbe55afca3117732e
Ilya Konstantinov <ilykonst95@gmail.com>
2020-06-07 11:34:59 +0400

    test(AuthLoginController.spec.ts): coverage 100%

--- spec/controllers/Auth/AuthLoginController.spec.ts
+++ spec/controllers/Auth/AuthLoginController.spec.ts
@@ -9,25 +9,74 @@ import bcrypt from "bcrypt";
 import {pwdSaltRounds} from "@/shared/constants";
 
 describe("AuthLoginController", () => {
-    test("login", async () => {
-        const userRepositoryMock = mock<IUserRepository>();
-        const password = "p";
+    describe("login", () => {
+        test("200", async () => {
+            const userRepositoryMock = mock<IUserRepository>();
+            const password = "p";
 
-        userRepositoryMock.findOne.mockResolvedValue(new User(
-            "test",
-            "email",
-            UserRoles.Standard,
-            bcrypt.hashSync(password, pwdSaltRounds),
-        ));
+            userRepositoryMock.findOne.mockResolvedValue(new User(
+                "test",
+                "email",
+                UserRoles.Standard,
+                bcrypt.hashSync(password, pwdSaltRounds),
+            ));
 
-        container.register("IUserRepository", {
-            useValue: userRepositoryMock,
+            container.register("IUserRepository", {
+                useValue: userRepositoryMock,
+            });
+
+            const res = await request(app)
+                .post("/api/auth/login")
+                .send({email: "test", password});
+
+            expect(res.status).toBe(200);
         });
 
-        const res = await request(app)
-            .post("/api/auth/login")
-            .send({email: "test", password});
+        test("400", async () => {
+            const userRepositoryMock = mock<IUserRepository>();
 
-        expect(res.status).toBe(200);
+            container.register("IUserRepository", {
+                useValue: userRepositoryMock,
+            });
+
+            const res = await request(app)
+                .post("/api/auth/login");
+
+            expect(res.status).toBe(400);
+        });
+
+        describe("401", () => {
+            test("user not found", async () => {
+                const userRepositoryMock = mock<IUserRepository>();
+
+                userRepositoryMock.findOne.mockResolvedValue(undefined);
+
+                container.register("IUserRepository", {
+                    useValue: userRepositoryMock,
+                });
+
+                const res = await request(app)
+                    .post("/api/auth/login")
+                    .send({email: "test", password: "password"});
+
+                expect(res.status).toBe(401);
+            });
+
+            test("wrong password", async () => {
+                const userRepositoryMock = mock<IUserRepository>();
+
+                userRepositoryMock.findOne.mockResolvedValue(new User());
+
+                container.register("IUserRepository", {
+                    useValue: userRepositoryMock,
+                });
+
+                const res = await request(app)
+                    .post("/api/auth/login")
+                    .send({email: "test", password: "password"});
+
+                expect(res.status).toBe(401);
+            });
+        });
     });
 })
